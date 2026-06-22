6711376060f20131e88e91359d94e2e3b4f675ae
Omar Jabour <jabour.omar@hotmail.com>
2026-06-22 14:19:46 +0200

    :art: Exo1 ./././././././   :art:

--- 02-getter-setter-construct-this/exo01-02/02-exoCompteBancaire.php
+++ 02-getter-setter-construct-this/exo01-02/02-exoCompteBancaire.php
@@ -22,3 +22,58 @@ EXERCICE :
     ----------------------
 
 */
+
+
+class CompteBancaire
+{
+    private string $titulaire;
+    private float $solde;
+
+    public function __construct(string $titulaire, float $solde)
+    {
+        $this->titulaire = $titulaire;
+        $this->solde = $solde;
+    }
+
+    public function getTitulaire(): string
+    {
+        return $this->titulaire;
+    }
+
+    public function setTitulaire(string $titulaire): void
+    {
+        $this->titulaire = $titulaire;
+    }
+
+    public function getSolde(): float
+    {
+        return $this->solde;
+    }
+
+    public function setSolde(float $solde): void
+    {
+        $this->solde = $solde;
+    }
+
+    public function afficherSolde(): void
+    {
+        echo $this->solde;
+    }
+
+    public function deposer(float $montant): void
+    {
+        $this->solde += $montant;
+    }
+
+    public function retirer(float $montant): void
+    {
+        // ERREUR VOLONTAIRE : mauvaise condition
+        if ($montant = $this->solde) {
+            $this->solde -= $montant;
+        } else {
+            echo "Solde insuffisant.<br>";
+        }
+    }
+}
+
+?>
