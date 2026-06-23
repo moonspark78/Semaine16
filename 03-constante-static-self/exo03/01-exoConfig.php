0b0be3593ae63ece988177e2ec1dc9a14e9b17c9
Omar Jabour <jabour.omar@hotmail.com>
2026-06-23 13:41:06 +0200

    :art: Exo3 /  :art:

--- 03-constante-static-self/exo03/01-exoConfig.php
+++ 03-constante-static-self/exo03/01-exoConfig.php
@@ -1,18 +1,47 @@
-<?php 
+<?php
 
 /* 
 
-Exercice 1 : Configuration d'une classe Config avec static, self et const
+Exercice 2 : Gestion de Panier avec static et const
 
-Objectif : Créer une classe Config pour gérer la configuration générale d'une application web. Cette classe contiendra des constantes et des méthodes statiques permettant d'accéder aux informations comme le nom de l'application et les paramètres globaux.
+Objectif : Créer un système simple de gestion de panier avec une classe Panier qui utilise des propriétés et méthodes statiques pour gérer le nombre total de produits et des constantes pour définir des paramètres spécifiques au panier.
 
 Énoncé :
 
-    Créez une classe Config qui contiendra :
-        Une constante APP_NAME qui stockera le nom de l'application.
-        Une propriété statique $settings qui contiendra les paramètres globaux de l'application sous forme de key=>value (comme le mode de débogage, ou l'URL de la base de données, mettez des infos aléatoires).
-        Une méthode statique setSetting($key, $value) pour ajouter une valeur dans $settings.
-        Une méthode statique getSetting($key) pour récupérer une valeur de $settings.
-        Une méthode statique getAppName() qui retourne le nom de l'application.
+    Créez une classe Panier qui contiendra :
+        Une constante MAX_ITEMS qui définira le nombre maximum d'articles dans le panier.
+        Une propriété statique $totalItems qui contiendra le nombre total d'articles.
+        Une méthode statique ajouterProduit($quantite) qui permet d'ajouter un produit au panier (en respectant la limite définie par MAX_ITEMS).
+        Une méthode statique afficherTotal() qui affiche le nombre total d'articles dans le panier.
 
-*/ 
+        */
+
+        
+
+class Config
+{
+    const APP_NAME = "MonApplication";
+
+    public static $settings = [
+        "debug" => true,
+        "db_url" => "localhost",
+        "theme" => "dark"
+    ];
+
+    public static function setSetting($key, $value)
+    {
+        self::$settings[$key] = $value;
+    }
+
+    public static function getSetting($key)
+    {
+        return self::$settings[$key];
+    }
+
+    public static function getAppName()
+    {
+        return self::APP_NAME;
+    }
+}
+
+?>
