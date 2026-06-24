6058e3e3120171ccc34df72fc707ed22a345b23f
Omar Jabour <jabour.omar@hotmail.com>
2026-06-24 09:54:02 +0200

    :art:Héritage : exo04-1  :art:

---
+++ 04-heritage/exo04/01-exoVehicule.php
@@ -0,0 +1,40 @@
+<?php
+
+
+/*
+
+       Exercice 1 : 
+        Modifier le code des classes et mettre en place les héritages pour répondre aux questions ci-dessous : 
+
+    1. Faire en sorte de ne pas avoir d'objet Vehicule
+    2. Obligation pour la Renault et la Peugeot de posséder exactement la même méthode démarrer()
+    3. Obligation pour la Renault de déclarer un carburant diesel et pour la Peugeot de déclarer un carburant essence 
+    4. La Renault doit effectuer 30 test de + qu'un véhicule de base 
+    5. La Peugeot doit effectuer 70 test de + qu'un vehicule de base
+    6. Effectuer les affichages nécessaires
+*/
+
+
+
+class Vehicule
+{
+
+    public function demarrer()
+    {
+        return 'je démarre';
+    }
+
+    public function carburant()
+    {
+        return "essence ? ou diesel ?";
+    }
+
+    public function nombreDeTestObligatoire()
+    {
+        return 100;
+    }
+}
+
+class Peugeot {}
+
+class Renault {}
