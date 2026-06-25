0a030e8f606d86651db70bfd10042194fce90aa3
Omar Jabour <jabour.omar@hotmail.com>
2026-06-25 17:06:13 +0200

    tp10  ./././.

--- 10-TP-Transformation/Classes/Config.php
+++ 10-TP-Transformation/Classes/Config.php
@@ -5,5 +5,5 @@ class Config
     public const DB_HOST = "localhost";
     public const DB_NAME = "tp_poo";
     public const DB_USER = "root";
-    public const DB_PASSWORD = "";
+    public const DB_PASSWORD = "root";
 }
