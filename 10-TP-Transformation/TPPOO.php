3069f796ed601d399f6fa46eb1a9d1ec95a57d96
Omar Jabour <jabour.omar@hotmail.com>
2026-06-25 15:07:23 +0200

    :art:Héritage : ttp10  :art:

---
+++ 10-TP-Transformation/TPPOO.php
@@ -0,0 +1,21 @@
+<?php 
+
+/* 
+
+    Reprendre les TPs Inscription et Connexion du Chapitre Post S14-PHP 
+    Refaire ces deux TPs en incluant la base de données (insert des inscriptions, et possibilité de s'identifier sur la page connexion)
+    Et également en les transformant au plus possible en PHP Orienté Objet
+
+    Faites en autant que vous le pouvez/souhaitez/trouvez judicieux.
+
+     Quelques tips : 
+        - Classe Utilisateur pour la compréhension des vérifications attendus en rapport avec le schéma de la table Utilisateur 
+        - Classe FormValidator pour les vérifs des saisies 
+        - Classe SessionManager plutôt que de manipuler $_SESSION à la main 
+        - Classe Config plutôt qu'un fichier config.php global 
+        - Classe qui manipule la BDD 
+        - Pensez à l'autoload 
+
+        - Pour aller plus loin, intégrer aussi le TP tchat de S15-MySQLSecu - PDO 
+
+*/
